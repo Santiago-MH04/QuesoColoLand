af0adbf7f092f588e9a3bfabaa201c3c7b042646
Santiago <santimh04@gmail.com>
2025-06-10 16:38:51 -0500

    feat: repository layer created. Jira ticker: ques-13

--- msvc-attractions/src/main/java/com/quesocololand/msvc_attractions/repositories/AttractionRepository.java
+++ msvc-attractions/src/main/java/com/quesocololand/msvc_attractions/repositories/AttractionRepository.java
@@ -1,4 +1,12 @@
 package com.quesocololand.msvc_attractions.repositories;
 
-public interface AttractionRepository {
+import com.quesocololand.msvc_attractions.models.Attraction;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface AttractionRepository extends MongoRepository<Attraction, String> {
+    //Atributos de AttractionRepository
+    //Constructores de AttractionRepository
+    //Asignadores de atributos de AttractionRepository (setters)
+    //Lectores de atributos de AttractionRepository (getters)
+    //Métodos de AttractionRepository
 }
