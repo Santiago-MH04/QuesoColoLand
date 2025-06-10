8f9593aa24dcfa766f102c852f6992f4528887dc
Santiago <santimh04@gmail.com>
2025-06-10 15:45:01 -0500

    feat: AttractionDTO object created. ques-16 ticket fulfilled

--- msvc-attractions/src/main/java/com/quesocololand/msvc_attractions/models/AttractionDTO.java
+++ msvc-attractions/src/main/java/com/quesocololand/msvc_attractions/models/AttractionDTO.java
@@ -1,4 +1,29 @@
 package com.quesocololand.msvc_attractions.models;
 
+import com.quesocololand.msvc_attractions.models.utils.AttractionStatus;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
 public class AttractionDTO {
+        //Atributos de AttractionDTO
+    private String id;
+    private String name;
+    private AttractionStatus status;
+    private int capacity;
+    private LocalDateTime lastUpdate;
+
+    //Constructores de AttractionDTO
+    //Asignadores de atributos de AttractionDTO (setters)
+    //Lectores de atributos de AttractionDTO (getters)
+    //Métodos de AttractionDTO
 }
