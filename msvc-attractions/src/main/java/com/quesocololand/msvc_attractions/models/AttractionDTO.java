b5abefcf699f0a17a097128e22306b9ca3a99565
Santiago Marín Higuita <146114200+Santiago-MH04@users.noreply.github.com>
2025-06-11 12:01:23 -0500

    Ques 16 (#3)
    * feat: project creation with microservice msvc-attractions and Eureka Server centralisation defined
    
    * feat: Rabbit MQ provided to microservice msvc-attractions. API Gateway routing added. Pull request template file created in order to fulfill task ques-19
    
    * fix: translation corrected on file pull_request_template.md
    
    * Fix: additional translation correction on pull_request_template.md file
    
    * fix: ques-19 ticket. Final translation correction on pull_request_template.md file
    
    * feat: AttractionDTO object created. ques-16 ticket fulfilled
    
    * fix: translation issues corrected

--- msvc-attractions/src/main/java/com/quesocololand/msvc_attractions/models/AttractionDTO.java
+++ msvc-attractions/src/main/java/com/quesocololand/msvc_attractions/models/AttractionDTO.java
@@ -1,4 +1,29 @@
 package com.quesocololand.msvc_attractions.models;
 
+import com.quesocololand.msvc_attractions.models.utils.AttractionStatus;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
 public class AttractionDTO {
+        //Fields of AttractionDTO
+    private LocalDateTime lastUpdate;
+    private String id;
+    private String name;
+    private AttractionStatus status;
+    private int capacity;
+
+    //Constructors of AttractionDTO
+    //Field setters of AttractionDTO (setters)
+    //Field getters of AttractionDTO (getters)
+    //Methods de AttractionDTO
 }
