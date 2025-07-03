caa04103a29dc3cd603d8bdbb27408f05f4b5cef
Santiago <santimh04@gmail.com>
2025-07-03 17:19:24 -0500

    feat: Swagger endpoints documented to msvc-attractions. Jira ticket ques23

--- msvc-attractions/src/main/java/com/quesocololand/msvcattractions/controllers/ExceptionController.java
+++ msvc-attractions/src/main/java/com/quesocololand/msvcattractions/controllers/ExceptionController.java
@@ -1,6 +1,11 @@
 package com.quesocololand.msvcattractions.controllers;
 
 import com.quesocololand.msvcattractions.exceptions.AttractionNotFoundException;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -12,9 +17,14 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestControllerAdvice
+@Tag(
+    name = "errors",
+    description = "exceptions controller"
+)
 public class ExceptionController {
     //Fields of ExceptionController
     //Constructors of ExceptionController
@@ -22,6 +32,21 @@ public class ExceptionController {
     //Field getters of ExceptionController (getters)
         //Methods of ExceptionController
     @ExceptionHandler({AttractionNotFoundException.class})
+    @Operation(
+        summary = "attraction not found exception handler",
+        description = "is thrown when an attraction in QuesoColoLand was not found",
+        tags = {"exception", "not found"},
+        responses = {
+            @ApiResponse(
+                responseCode = "404",
+                description = "attraction searching failed",
+                content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = com.quesocololand.msvcattractions.models.dto.Error.class)
+                )
+            )
+        }
+    )
     public ResponseEntity<com.quesocololand.msvcattractions.models.dto.Error> notFoundException(Exception e){
         com.quesocololand.msvcattractions.models.dto.Error error = new com.quesocololand.msvcattractions.models.dto.Error();
             error.setName("Attraction searching error");
@@ -34,6 +59,21 @@ public class ExceptionController {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @Operation(
+        summary = "attraction not created exception handler",
+        description = "is thrown when an attraction in QuesoColoLand was not properly created",
+        tags = {"exception", "bad creation", "insufficient parameters"},
+        responses = {
+            @ApiResponse(
+                responseCode = "400",
+                description = "attraction creation failed",
+                content = @Content(
+                    mediaType = "application/json",
+                    schema = @Schema(implementation = Map.class)
+                )
+            )
+        }
+    )
     public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException e) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy 'T' HH:mm:ss");
         String formattedTimestamp = LocalDateTime.now().format(formatter);
