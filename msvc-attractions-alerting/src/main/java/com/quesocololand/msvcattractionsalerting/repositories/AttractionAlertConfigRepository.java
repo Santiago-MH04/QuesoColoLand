fb94961bceb72788ae62a0dbe152d972f3c2f307
Santiago <santimh04@gmail.com>
2025-07-29 13:25:36 -0500

    feat: repository layer to msvc-attractions-alert completed. Jira ticket ques6

---
+++ msvc-attractions-alerting/src/main/java/com/quesocololand/msvcattractionsalerting/repositories/AttractionAlertConfigRepository.java
@@ -0,0 +1,16 @@
+package com.quesocololand.msvcattractionsalerting.repositories;
+
+import com.quesocololand.msvcattractionsalerting.models.AttractionAlertConfig;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.Optional;
+
+public interface AttractionAlertConfigRepository extends MongoRepository<AttractionAlertConfig, String> {
+    //Fields of AttractionAlertConfigRepository
+    //Constructors of AttractionAlertConfigRepository
+    //Field setters of AttractionAlertConfigRepository (setters)
+    //Field getters of AttractionAlertConfigRepository (getters)
+    //Methods of AttractionAlertConfigRepository
+    public Optional<AttractionAlertConfig> findByAttractionId(String attractionId);
+    public Optional<AttractionAlertConfig> findByIsGlobal(boolean isGlobal);
+}
